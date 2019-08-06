0660b7694845968a49a9b5c691792f99385b7720
William D. Yurkow <bcwill42@gmail.com>
2019-08-05 20:03:39 -0400

    added all functions

--- liri.js
+++ liri.js
@@ -71,7 +71,68 @@ for (var i = 0; i < jsonData.length; i++) {
 }
 
   }
-)
+);
+};
+
+var getMovie = function(movieName) {
+if (movieName === undefined) {
+  movieName = "Mr Nobody";
 }
 
-getMyBands("greta van fleet")
+var urlHit =
+"http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+axios.get(urlHit).then(
+  function(response) {
+    var jsonData = response.data;
+
+    console.log("Title; " + jsonData.Title);
+    console.log("Year:" + jsonData.Year);
+    console.log("Rated: " + jsonData.Rated);
+    console.log("IMDB Rating: " + jsonData.imdbRating);
+    console.log("Country: " + jsonData.Country);
+    console.log("Language: " + jsonData.Language);
+    console.log("Plot:" + jsonData.Plot);
+    console.log("Actors: " + jsonData.Actors);
+    console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[i].Value);
+
+  }
+);
+
+};
+
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data){
+    console.log(data);
+
+    var dataArr = data.split(",");
+
+    if(dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+
+    }
+  
+  });
+};
+
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case "concert-this":
+      getMyBands(functionData);
+      break;
+      case "spotify-this-song":
+      getMeSpotify(functionData);
+      break; 
+      case getMovie(functionData):
+      break;
+      case "do-what-it-says":
+      doWhatItSays();
+      break;
+      default:
+      console.log("Liri doesnt know that");
+        }
+};
+
+pick(process.argv[2], process.argv.slice(3).join(" "));
